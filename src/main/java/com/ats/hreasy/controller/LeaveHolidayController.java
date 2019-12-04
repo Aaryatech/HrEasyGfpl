3d80346268ddf62299e3ba2bc24c7ac8c6d73891
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-12-04 15:22:58 +0530

    /Akshay

---
+++ src/main/java/com/ats/hreasy/controller/LeaveHolidayController.java
@@ -0,0 +1,423 @@
+package com.ats.hreasy.controller;
+
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ats.hreasy.common.Constants;
+import com.ats.hreasy.common.DateConvertor;
+import com.ats.hreasy.common.FormValidation;
+import com.ats.hreasy.model.CalenderYear;
+import com.ats.hreasy.model.GetHoliday;
+import com.ats.hreasy.model.Holiday;
+import com.ats.hreasy.model.Info;
+import com.ats.hreasy.model.Location;
+
+@Controller
+@Scope("session")
+public class LeaveHolidayController {
+	Holiday editHoliday = new Holiday();
+
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	Date now = new Date();
+	String curDate = dateFormat.format(new Date());
+	String dateTime = dateFormat.format(now);
+
+	@RequestMapping(value = "/holidayAdd", method = RequestMethod.GET)
+	public ModelAndView holidayAdd(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpSession session = request.getSession();
+		ModelAndView model = null;
+
+		try {
+
+			/*
+			 * List<AccessRightModule> newModuleList = (List<AccessRightModule>)
+			 * session.getAttribute("moduleJsonList"); Info view =
+			 * AcessController.checkAccess("holidayAdd", "showHolidayList",0, 1,0, 0,
+			 * newModuleList);
+			 * 
+			 * if (view.isError() == true) {
+			 * 
+			 * model = new ModelAndView("accessDenied");
+			 * 
+			 * } else {
+			 */
+			model = new ModelAndView("master/holiday");
+
+			// LoginResponse userObj = (LoginResponse) session.getAttribute("UserDetail");
+
+			MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+			map.add("companyId", 1);
+			Location[] location = Constants.getRestTemplate().postForObject(Constants.url + "/getLocationList", map,
+					Location[].class);
+
+			List<Location> locationList = new ArrayList<Location>(Arrays.asList(location));
+
+			for (int i = 0; i < locationList.size(); i++) {
+
+				locationList.get(i).setExVar1(FormValidation.Encrypt(String.valueOf(locationList.get(i).getLocId())));
+			}
+
+			model.addObject("locationList", locationList);
+
+			// }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/submitInsertHoliday", method = RequestMethod.POST)
+	public String submitInsertHoliday(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+			HttpSession session = request.getSession();
+			// LoginResponse userObj = (LoginResponse) session.getAttribute("UserDetail");
+
+			CalenderYear calculateYear = Constants.getRestTemplate()
+					.getForObject(Constants.url + "/getCalculateYearListIsCurrent", CalenderYear.class);
+
+			String dateRange = request.getParameter("dateRange");
+			String[] arrOfStr = dateRange.split("to", 2);
+
+			String holidayRemark = request.getParameter("holidayRemark");
+			String holidayTitle = request.getParameter("holidayTitle");
+
+			String[] locIds = request.getParameterValues("locId");
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < locIds.length; i++) {
+				sb = sb.append(locIds[i] + ",");
+
+			}
+			String locIdList = sb.toString();
+			locIdList = locIdList.substring(0, locIdList.length() - 1);
+
+			int holidayId = 0;
+			try {
+				holidayId = Integer.parseInt(request.getParameter("holidayId"));
+			} catch (Exception e) {
+				holidayId = 0;
+			}
+
+			Boolean ret = false;
+
+			if (FormValidation.Validaton(dateRange, "") == true) {
+
+				ret = true;
+
+			}
+
+			if (FormValidation.Validaton(holidayRemark, "") == true) {
+
+				ret = true;
+
+			}
+
+			if (ret == false) {
+
+				Holiday holiday = new Holiday();
+
+				holiday.setCalYrId(calculateYear.getCalYrId());
+				holiday.setCompanyId(1);
+				holiday.setDelStatus(1);
+
+				holiday.setExVar1("NA");
+				holiday.setExVar2(holidayTitle);
+				holiday.setExVar3("NA");
+				holiday.setHolidayFromdt(DateConvertor.convertToYMD(arrOfStr[0].toString().trim()));
+				holiday.setHolidayTodt(DateConvertor.convertToYMD(arrOfStr[1].toString().trim()));
+
+				holiday.setHolidayRemark(holidayRemark);
+				holiday.setIsActive(1);
+				holiday.setLocId(locIdList);
+				holiday.setMakerEnterDatetime(dateTime);
+				holiday.setMakerUserId(1);
+
+				Holiday res = Constants.getRestTemplate().postForObject(Constants.url + "/saveHoliday", holiday,
+						Holiday.class);
+
+				if (res.isError() == false) {
+					session.setAttribute("successMsg", "Record Inserted Successfully");
+				} else {
+					session.setAttribute("errorMsg", "Failed to Insert Record");
+				}
+
+			} else {
+				session.setAttribute("errorMsg", "Failed to Insert Record");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/showHolidayList";
+	}
+
+	@RequestMapping(value = "/editHoliday", method = RequestMethod.GET)
+	public ModelAndView editHoliday(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = null;
+		HttpSession session = request.getSession();
+
+		try {
+
+			/*
+			 * List<AccessRightModule> newModuleList = (List<AccessRightModule>)
+			 * session.getAttribute("moduleJsonList"); Info view =
+			 * AcessController.checkAccess("editHoliday", "showHolidayList", 0, 0, 1, 0,
+			 * newModuleList);
+			 * 
+			 * if (view.isError() == true) {
+			 * 
+			 * model = new ModelAndView("accessDenied");
+			 * 
+			 * } else {
+			 */
+			model = new ModelAndView("master/holiday_edit");
+
+			// LoginResponse userObj = (LoginResponse) session.getAttribute("UserDetail");
+
+			MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+			map.add("companyId", 1);
+
+			Location[] location = Constants.getRestTemplate().postForObject(Constants.url + "/getLocationList", map,
+					Location[].class);
+
+			List<Location> locationList = new ArrayList<Location>(Arrays.asList(location));
+
+			for (int i = 0; i < locationList.size(); i++) {
+
+				locationList.get(i).setExVar1(FormValidation.Encrypt(String.valueOf(locationList.get(i).getLocId())));
+			}
+
+			model.addObject("locationList", locationList);
+
+			// getCalculateYearList
+
+			/*
+			 * CalenderYear[] calculateYear = Constants.getRestTemplate()
+			 * .getForObject(Constants.url + "/getCalculateYearListIsCurrent",
+			 * CalenderYear[].class);
+			 * 
+			 * List<CalenderYear> yearList = new ArrayList<>(Arrays.asList(calculateYear));
+			 * 
+			 * model.addObject("yearList", yearList);
+			 */
+			String base64encodedString = request.getParameter("holidayId");
+			String holidayId = FormValidation.DecodeKey(base64encodedString);
+
+			map = new LinkedMultiValueMap<>();
+			map.add("holidayId", holidayId);
+			editHoliday = Constants.getRestTemplate().postForObject(Constants.url + "/getHolidayById", map,
+					Holiday.class);
+			model.addObject("editHoliday", editHoliday);
+			System.out.println(editHoliday);
+			editHoliday.setHolidayFromdt(DateConvertor.convertToDMY(editHoliday.getHolidayFromdt()));
+			editHoliday.setHolidayTodt(DateConvertor.convertToDMY(editHoliday.getHolidayTodt()));
+			System.out.println(editHoliday);
+			List<Integer> locIdList = Stream.of(editHoliday.getLocId().split(",")).map(Integer::parseInt)
+					.collect(Collectors.toList());
+
+			model.addObject("locIdList", locIdList);
+			// }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/showHolidayList", method = RequestMethod.GET)
+	public ModelAndView showHolidayList(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = null;
+		HttpSession session = request.getSession();
+
+		try {
+			/*LoginResponse userObj = (LoginResponse) session.getAttribute("UserDetail");
+			List<AccessRightModule> newModuleList = (List<AccessRightModule>) session.getAttribute("moduleJsonList");
+			Info view = AcessController.checkAccess("showHolidayList", "showHolidayList", 1, 0, 0, 0, newModuleList);
+
+			if (view.isError() == true) {
+
+				model = new ModelAndView("accessDenied");
+
+			} else {*/
+
+				model = new ModelAndView("master/holiday_list");
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("companyId", 1);
+
+				GetHoliday[] holListArray = Constants.getRestTemplate().postForObject(Constants.url + "/getHolidayList",
+						map, GetHoliday[].class);
+
+				List<GetHoliday> holList = new ArrayList<>(Arrays.asList(holListArray));
+
+				for (int i = 0; i < holList.size(); i++) {
+
+					holList.get(i).setExVar1(FormValidation.Encrypt(String.valueOf(holList.get(i).getHolidayId())));
+				}
+				
+				model.addObject("addAccess", 0);
+				model.addObject("editAccess", 0);
+				model.addObject("deleteAccess", 0);
+				model.addObject("holList", holList);
+				/*Info add = AcessController.checkAccess("showHolidayList", "showHolidayList", 0, 1, 0, 0, newModuleList);
+				Info edit = AcessController.checkAccess("showHolidayList", "showHolidayList", 0, 0, 1, 0,
+						newModuleList);
+				Info delete = AcessController.checkAccess("showHolidayList", "showHolidayList", 0, 0, 0, 1,
+						newModuleList);
+
+				if (add.isError() == false) {
+					System.out.println(" add   Accessable ");
+					model.addObject("addAccess", 0);
+
+				}
+				if (edit.isError() == false) {
+					System.out.println(" edit   Accessable ");
+					model.addObject("editAccess", 0);
+				}
+				if (delete.isError() == false) {
+					System.out.println(" delete   Accessable ");
+					model.addObject("deleteAccess", 0);
+
+				}*/
+				// System.out.println("HolidayList" + holList.toString());
+			//}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/submitEditHoliday", method = RequestMethod.POST)
+	public String submitEditHoliday(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpSession session = request.getSession();
+
+		try {
+
+			String dateRange = request.getParameter("dateRange");
+			String[] arrOfStr = dateRange.split("to", 2);
+
+			String holidayRemark = request.getParameter("holidayRemark");
+			String holidayTitle = request.getParameter("holidayTitle");
+
+			String[] locIds = request.getParameterValues("locId");
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < locIds.length; i++) {
+				sb = sb.append(locIds[i] + ",");
+
+			}
+			String locIdList = sb.toString();
+			locIdList = locIdList.substring(0, locIdList.length() - 1);
+
+			Boolean ret = false;
+
+			if (FormValidation.Validaton(dateRange, "") == true) {
+
+				ret = true;
+
+			}
+
+			if (FormValidation.Validaton(holidayTitle, "") == true) {
+
+				ret = true;
+
+			}
+
+			if (ret == false) {
+
+				editHoliday.setHolidayFromdt(DateConvertor.convertToYMD(arrOfStr[0].toString().trim()));
+				editHoliday.setHolidayTodt(DateConvertor.convertToYMD(arrOfStr[1].toString().trim()));
+				editHoliday.setHolidayRemark(holidayRemark);
+				editHoliday.setLocId(locIdList);
+				editHoliday.setExVar2(holidayTitle);
+				Holiday res = Constants.getRestTemplate().postForObject(Constants.url + "/saveHoliday", editHoliday,
+						Holiday.class);
+
+				if (res.isError() == false) {
+					session.setAttribute("successMsg", "Record Updated Successfully");
+				} else {
+					session.setAttribute("errorMsg", "Failed to Update Record");
+				}
+
+			} else {
+				session.setAttribute("errorMsg", "Failed to Update Record");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("errorMsg", "Failed to Update Record");
+		}
+
+		return "redirect:/showHolidayList";
+	}
+
+	@RequestMapping(value = "/deleteHoliday", method = RequestMethod.GET)
+	public String deleteHoliday(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpSession session = request.getSession();
+		String a = null;
+		
+
+		try {
+			/*List<AccessRightModule> newModuleList = (List<AccessRightModule>) session.getAttribute("moduleJsonList");
+			Info view = AcessController.checkAccess("deleteHoliday", "showHolidayList", 0, 0, 0, 1, newModuleList);
+			if (view.isError() == true) {
+
+				a = "redirect:/accessDenied";
+
+			} else {*/
+				a = "redirect:/showHolidayList";
+
+				String base64encodedString = request.getParameter("holidayId");
+				String holidayId = FormValidation.DecodeKey(base64encodedString);
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("holidayId", holidayId);
+				Info info = Constants.getRestTemplate().postForObject(Constants.url + "/deleteHoliday", map,
+						Info.class);
+
+				if (info.isError() == false) {
+					session.setAttribute("successMsg", "Deleted Successfully");
+				} else {
+					session.setAttribute("errorMsg", "Failed to Delete");
+				}
+			//}
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("errorMsg", "Failed to Delete");
+		}
+		return a;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/master/holiday.jsp
@@ -0,0 +1,348 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<jsp:include page="/WEB-INF/views/include/metacssjs.jsp"></jsp:include>
+</head>
+
+<body>
+
+	<!-- Main navbar -->
+	<jsp:include page="/WEB-INF/views/include/header.jsp"></jsp:include>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		<!-- Main sidebar -->
+		<jsp:include page="/WEB-INF/views/include/left.jsp"></jsp:include>
+		<!-- /main sidebar -->
+
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Page header -->
+			<div class="page-header page-header-light">
+
+<%-- 
+				<div
+					class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+					<div class="d-flex">
+						<div class="breadcrumb">
+							<a href="index.html" class="breadcrumb-item"><i
+								class="icon-home2 mr-2"></i> Home</a> <span
+								class="breadcrumb-item active">Dashboard</span>
+						</div>
+
+						<a href="#" class="header-elements-toggle text-default d-md-none"><i
+							class="icon-more"></i></a>
+					</div>
+
+					<div class="breadcrumb justify-content-center">
+						<a href="${pageContext.request.contextPath}/showHolidayList"
+							class="breadcrumb-elements-item"> Holiday List</a>
+
+					</div>
+
+
+				</div> --%>
+			</div>
+			<!-- /page header -->
+
+
+			<!-- Content area -->
+			<div class="content">
+
+				<!-- Form validation -->
+				<div class="row">
+					<div class="col-md-12">
+						<!-- Title -->
+						<!-- <div class="mb-3">
+							<h6 class="mb-0 font-weight-semibold">Hidden labels</h6>
+							<span class="text-muted d-block">Inputs with empty values</span>
+						</div> -->
+						<!-- /title -->
+
+
+						<div class="card">
+							 
+							<div class="card-header header-elements-inline">
+ 						<table width="100%">
+							<tr width="100%">
+								<td width="60%"><h5 class="card-title">Add Holiday</h5></td>
+								<td width="40%" align="right">
+							  <%-- 
+								 <a
+									href="${pageContext.request.contextPath}/holidayAdd"
+									class="breadcrumb-elements-item">
+										<button type="button" class="btn btn-primary">Add Holiday</button>
+								</a> --%> </td>
+							</tr>
+						</table>
+					</div>
+							<div class="card-body">
+								<%
+									if (session.getAttribute("errorMsg") != null) {
+								%>
+								<div
+									class="alert bg-danger text-white alert-styled-left alert-dismissible">
+									<button type="button" class="close" data-dismiss="alert">
+										<span>×</span>
+									</button>
+									<span class="font-weight-semibold">Oh snap!</span>
+									<%
+										out.println(session.getAttribute("errorMsg"));
+									%>
+								</div>
+
+								<%
+									session.removeAttribute("errorMsg");
+									}
+								%>
+								<%
+									if (session.getAttribute("successMsg") != null) {
+								%>
+								<div
+									class="alert bg-success text-white alert-styled-left alert-dismissible">
+									<button type="button" class="close" data-dismiss="alert">
+										<span>×</span>
+									</button>
+									<span class="font-weight-semibold">Well done!</span>
+									<%
+										out.println(session.getAttribute("successMsg"));
+									%>
+								</div>
+								<%
+									session.removeAttribute("successMsg");
+									}
+								%>
+
+								<form
+									action="${pageContext.request.contextPath}/submitInsertHoliday"
+									id="submitInsertHoli" method="post">
+
+									<div class="form-group row">
+										<label class="col-form-label col-lg-2" for="holidayTitle">Holiday
+											Title <span style="color:red">* </span>:</label>
+										<div class="col-lg-10">
+											<input type="text" class="form-control"
+												placeholder="Enter Holiday Title" id="holidayTitle"
+												name="holidayTitle" autocomplete="off" onchange="trim(this)"
+												maxlength="100"> <span
+												class="validation-invalid-label" id="error_holidayTitle"
+												style="display: none;">This field is required.</span>
+										</div>
+									</div>
+
+
+
+									<div class="form-group row">
+										<label class="col-form-label col-lg-2" for="locId">Select
+											Location <span style="color:red">* </span>:</label>
+										<div class="col-lg-10">
+											<select name="locId" data-placeholder="Select Location"
+												id="locId" multiple="multiple"
+												class="form-control form-control-sm select"
+												data-container-css-class="select-sm" data-fouc>
+											<option value="">Select Location</option>
+												<c:forEach items="${locationList}" var="location">
+													<option value="${location.locId}">${location.locName}</option>
+												</c:forEach>
+											</select> <span class="validation-invalid-label" id="error_locationId"
+												style="display: none;">This field is required.</span>
+										</div>
+									</div>
+
+
+									<%-- <div class="form-group row">
+										<label class="col-form-label col-lg-2" for="calYrId">Select
+											year :*</label>
+										<div class="col-lg-10">
+											<select name="calYrId" data-placeholder="Select Year"
+												id="calYrId"
+												class="form-control form-control-select2 select2-hidden-accessible"
+												tabindex="-1" aria-hidden="true">
+												<option value="">Select Year</option>
+												<c:forEach items="${yearList}" var="year">
+
+													<option value="${year.calYrId}">${year.calYrFromDate}_${year.calYrToDate}</option>
+
+												</c:forEach>
+											</select> <span class="validation-invalid-label" id="error_calYrId"
+												style="display: none;">This field is required.</span>
+										</div>
+									</div>
+ --%>
+									<div class="form-group row">
+										<label class="col-form-label col-lg-2">Date Range <span style="color:red">* </span>:</label>
+										<div class="col-lg-10">
+											<input type="text" class="form-control daterange-basic_new "
+												value="21-04-2019 @ 21-05-2019" name="dateRange"
+												data-placeholder="Select Date" id="dateRange"> <span
+												class="validation-invalid-label" id="error_Range"
+												style="display: none;">This field is required.</span>
+
+										</div>
+									</div>
+
+									<div class="form-group row">
+										<label class="col-form-label col-lg-2" for="remark">
+											Remark : </label>
+										<div class="col-lg-10">
+											<textarea rows="3" cols="3" class="form-control"
+												placeholder="Any Remark for Holiday" onchange="trim(this)"
+												id="holidayRemark" name="holidayRemark">-</textarea>
+
+										</div>
+									</div>
+
+									<div class="form-group row mb-0">
+										<div class="col-lg-10 ml-lg-auto">
+
+											<button type="submit" class="btn bg-blue ml-3 legitRipple"
+												id="submtbtn">
+												Submit <i class="icon-paperplane ml-2"></i>
+											</button>
+											<a href="${pageContext.request.contextPath}/showHolidayList"><button
+										type="button" class="btn btn-primary"> Cancel</button></a>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+
+			</div>
+			<!-- /content area -->
+
+
+			<!-- Footer -->
+			<jsp:include page="/WEB-INF/views/include/footer.jsp"></jsp:include>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+	<script>
+		function trim(el) {
+			el.value = el.value.replace(/(^\s*)|(\s*$)/gi, ""). // removes leading and trailing spaces
+			replace(/[ ]{2,}/gi, " "). // replaces multiple spaces with one space 
+			replace(/\n +/, "\n"); // Removes spaces after newlines
+			return;
+		}
+
+		function validateEmail(email) {
+
+			var eml = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+			if (eml.test($.trim(email)) == false) {
+
+				return false;
+
+			}
+
+			return true;
+
+		}
+		function validateMobile(mobile) {
+			var mob = /^[1-9]{1}[0-9]{9}$/;
+
+			if (mob.test($.trim(mobile)) == false) {
+
+				//alert("Please enter a valid email address .");
+				return false;
+
+			}
+			return true;
+
+		}
+		$(document).ready(function($) {
+
+			$("#submitInsertHoli").submit(function(e) {
+				var isError = false;
+				var errMsg = "";
+
+				if (!$("#holidayTitle").val()) {
+
+					isError = true;
+
+					$("#error_holidayTitle").show()
+
+				} else {
+					$("#error_holidayTitle").hide()
+				}
+
+				if ($("#locId").val()=="") {
+
+					isError = true;
+
+					$("#error_locationId").show()
+
+				} else {
+					$("#error_locationId").hide()
+				}
+
+			
+				if (!$("#dateRange").val()) {
+
+					isError = true;
+
+					$("#error_Range").show()
+
+				} else {
+					$("#error_Range").hide()
+				}
+
+				if (!isError) {
+
+					var x =true ;
+					if (x == true) {
+
+						document.getElementById("submtbtn").disabled = true;
+						return true;
+					}
+					//end ajax send this to php page
+				}
+				return false;
+			});
+		});
+		//
+	</script>
+
+	<script type="text/javascript">
+		// Single picker
+		$('.datepickerclass').daterangepicker({
+			singleDatePicker : true,
+			selectMonths : true,
+			selectYears : true,
+			locale : {
+				format : 'DD-MM-YYYY'
+			}
+		});
+
+		//daterange-basic_new
+		// Basic initialization
+		$('.daterange-basic_new').daterangepicker({
+			applyClass : 'bg-slate-600',
+
+			cancelClass : 'btn-light',
+			locale : {
+				format : 'DD-MM-YYYY',
+				separator : ' to '
+			}
+		});
+	</script>
+
+</body>
+</html>
