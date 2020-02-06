2ab2c0c4205655f3907e81ade8a1b5d31cd090c6
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-02-06 16:57:59 +0530

    /Akshay

--- src/main/java/com/ats/hreasy/common/Constants.java
+++ src/main/java/com/ats/hreasy/common/Constants.java
@@ -1,27 +1,26 @@
-
-
-
 package com.ats.hreasy.common;
-  
+
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 public class Constants {
- 
-	public static final String url="http://localhost:8094/";
- 	public static final String imageSaveUrl = "/home/lenovo/Downloads/";
-	public static final String getImageSaveUrl = "/home/lenovo/Downloads/";
-	public static final String REPORT_SAVE = "/home/lenovo/pdf/abc.pdf";
+
+	//public static final String url = "http://localhost:8094/";
+	public static final String url="http://107.180.95.11:8080/HrEsayWebApi/";
+	public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
+	public static final String imageSaveUrl = "/home/lenovo/Documents/attendance/";
+	public static final String imageShowUrl = "http://localhost:8080/attendance/";
+	public static final String ReportURL = "http://localhost:8080/hreasy/";//gfpl
 	public static RestTemplate rest = new RestTemplate();
-	public static String[] allextension; 
-	public static String[] values = { "jpg", "jpeg", "gif", "png" };
-	 
-	 public static RestTemplate getRestTemplate() {
-		rest=new RestTemplate();
+	public static String[] allextension = { "txt", "doc", "pdf", "xls", "jpg", "jpeg", "gif", "png" };
+	public static String[] values;
+	public static Object getImageSaveUrl;
+
+	public static RestTemplate getRestTemplate() {
+		rest = new RestTemplate();
 		rest.getInterceptors().add(new BasicAuthorizationInterceptor("aaryatech", "Aaryatech@1cr"));
 		return rest;
 
-		} 
-	 
-	
+	}
+
 }
